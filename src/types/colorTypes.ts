3cf4c9ec0f0c98e27480ad6bc09abeab1c821705
Rimon <rimons2804@gmail.com>
2023-01-07 14:56:05 +0200

    color format types

---
+++ src/types/colorTypes.ts
@@ -0,0 +1,4 @@
+export type hex = string
+export type rgb = { r: number; g: number; b: number }
+export type hsl = { h: number; s: number; l: number }
+export type hsv = { h: number; s: number; v: number }
