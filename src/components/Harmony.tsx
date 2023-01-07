3681f87bf7dce9f98f640291f0e3128fbfa147aa
Rimon <rimons2804@gmail.com>
2023-01-07 14:55:44 +0200

    harmony cmp - present harmony colors

---
+++ src/components/Harmony.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import useHarmony from '../hooks/useHarmony'
+type HarmonyProps = {
+   type: string
+   color: string
+}
+
+const Harmony: React.FC<HarmonyProps> = ({ type, color }) => {
+   const harmony = useHarmony(color, type)
+
+   return (
+      <div className="harmony">
+         {harmony.map(color => (
+            <div className="color" style={{ backgroundColor: color }} />
+         ))}
+      </div>
+   )
+}
+
+export default Harmony
