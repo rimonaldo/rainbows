f744fb1714261ac176ecd0065067785aea9ea7ad
Rimon <rimons2804@gmail.com>
2023-01-07 14:55:14 +0200

    useHarmony hook to set harmony type and values

---
+++ src/hooks/useHarmony.tsx
@@ -0,0 +1,30 @@
+import { useState, useEffect } from 'react'
+import { getTriadic, getMonochromatic, getComplementary } from '../services/color.service'
+const useHarmony = (color: string, type: string) => {
+   const [harmony, setHarmony] = useState<string[]>([])
+
+   useEffect(() => {
+      let colors: string[] = []
+
+      switch (type) {
+         case 'monochromatic':
+            colors = getMonochromatic(color)
+            break
+         case 'triadic':
+            colors = getTriadic(color)
+            break
+         case 'complementary':
+            colors = getComplementary(color)
+            break
+         // case 'analogous':
+         //    colors = getAnalogous(color)
+         //    break
+      }
+
+      setHarmony(colors)
+   }, [color, type])
+
+   return harmony
+}
+
+export default useHarmony
