0cb222aa7718516596762e5d2c19ced0e02fc7d6
Rimon <rimons2804@gmail.com>
2023-01-17 17:18:36 +0200

    hex color input works

--- src/components/ColorVals.tsx
+++ src/components/ColorVals.tsx
@@ -1,67 +1,77 @@
 import { ColorType } from '../types/color'
-
+import { Color } from '../services/color.class'
+import { useState, useEffect } from 'react'
+import { HexColorInput } from 'react-colorful'
 interface ColorInputProps {
-   color: ColorType
-   onChange: (event: React.ChangeEvent<HTMLInputElement>) => void
+   hex: string
+   onChange: (hex: string) => void
 }
 
-const ColorVals: React.FunctionComponent<ColorInputProps> = ({ color, onChange }) => {
+const ColorVals: React.FunctionComponent<ColorInputProps> = ({ onChange, hex }) => {
+   const [color, setColor] = useState<ColorType>(new Color(hex))
+
+   useEffect(() => {
+      let newColor: ColorType = new Color(hex)
+      setColor(newColor)
+   }, [hex])
+
    return (
       <div className="color-vals">
          <div className="hex">
             <div className="ball" style={{ backgroundColor: color.hex }}></div>
-            <input type="text" value={color.hex} onChange={onChange} name="" />
+            {/* <input type="text" value={color.hex} onChange={onChange} name="" /> */}
+            <div className="hex-text">
+               <span>#</span>
+               <HexColorInput color={color.hex} onChange={onChange} />
+            </div>
          </div>
 
+         <div className="box"></div>
 
-            <div className="box"></div>
-
-  
-
-         <div className="val rgb">
+         {/* <div className="val rgb">
             <div>
                <label>r</label>
-               <input type="number" value={color.rgb.r} onChange={onChange} />
+               <input type="number" value={color.rgb.r}  />
             </div>
             <div>
                <label>g</label>
-               <input type="number" value={color.rgb.g} onChange={onChange} />
+               <input type="number" value={color.rgb.g}  />
             </div>
             <div>
                <label>b</label>
-               <input type="number" value={color.rgb.b} onChange={onChange} />
+               <input type="number" value={color.rgb.b}  />
             </div>
          </div>
 
          <div className="val hsl">
             <div>
                <label>h</label>
-               <input type="number" value={color.hsl.h.toFixed(0)} onChange={onChange} />
+               <input type="number" value={color.hsl.h.toFixed(0)}  />
             </div>
             <div>
                <label>s</label>
-               <input type="number" value={(color.hsl.s * 100).toFixed(0)} onChange={onChange} />
+               <input type="number" value={(color.hsl.s * 100).toFixed(0)}  />
             </div>
             <div>
                <label>l</label>
-               <input type="number" value={(color.hsl.l * 100).toFixed(0)} onChange={onChange} />
+               <input type="number" value={(color.hsl.l * 100).toFixed(0)}  />
             </div>
          </div>
 
          <div className="val hsv">
             <div>
                <label>h</label>
-               <input type="number" value={color.hsv.h} onChange={onChange} />
+               <input type="number" value={color.hsv.h}  />
             </div>
             <div>
                <label>s</label>
-               <input type="number" value={(color.hsv.s * 100).toFixed(0)} onChange={onChange} />
+               <input type="number" value={(color.hsv.s * 100).toFixed(0)}  />
             </div>
             <div>
                <label>v</label>
-               <input type="number" value={(color.hsv.v * 100).toFixed(0)} onChange={onChange} />
+               <input type="number" value={(color.hsv.v * 100).toFixed(0)}  />
             </div>
-         </div>
+         </div> */}
       </div>
    )
 }
