ba2fe689c09cb7b68bafa73953784fbbfa90c187
Rimon <rimons2804@gmail.com>
2023-01-17 13:38:13 +0200

    harmony's colors flex animation and hex displayd

---
+++ src/components/HarmonyColors.tsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import { ColorType } from '../types/color'
+type HarmonyProps = {
+   colors: string[]
+}
+
+const Harmony: React.FC<HarmonyProps> = ({ colors }) => {
+   return (
+      <div className="harmony-colors">
+         {colors?.map(color => (
+            <div className="color" style={{ background: color }}>
+               <div className="hex">
+                  <span >{color}</span>
+               </div>
+            </div>
+         ))}
+      </div>
+   )
+}
+
+export default Harmony
