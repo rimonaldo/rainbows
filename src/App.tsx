17eb830a4f7c6348153db51936a4feb0a4f8adef
Rimon <rimons2804@gmail.com>
2023-01-18 11:27:56 +0200

    cleaned main app

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,9 @@ import React, { useState, useEffect, useRef } from 'react'
 import ColorContainer from './layouts/ColorContainer'
 
 function App() {
-   const [harmonyType, setHarmonyType] = useState('monochromatic')
-   const [harmonyColors, setHarmonyColors] = useState<string[]>([])
 
    return (
       <div className="App">
-         <h1 style={{ color: harmonyType === 'monochromatic' ? harmonyColors[0] : harmonyColors[1] }}>Rainbows</h1>
          <ColorContainer />
       </div>
    )
