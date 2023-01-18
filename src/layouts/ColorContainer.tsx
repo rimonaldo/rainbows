2a77405229eb48498fce33c339aaf6a1ed0e5e62
Rimon <rimons2804@gmail.com>
2023-01-18 11:43:06 +0200

    started adressing improved hsl setting logic

--- src/layouts/ColorContainer.tsx
+++ src/layouts/ColorContainer.tsx
@@ -10,24 +10,23 @@ import HarmonyColors from '../components/HarmonyColors'
 import { hsl } from '../types/colorTypes'
 import Title from '../components/HarmonyTitle'
 import ColorName from '../components/ColorName'
-import Picker from '../components/Picker'
 import { hslToRgb, rgbToHex } from '../services/colorService'
 const ColorContainer: React.FC = () => {
    const [color, setColor] = useState<ColorType>(new Color('#ffffff')),
+      [hex, setHex] = useState<string>(color.hex),
       [selectedHarmony, setSelectedHarmony] = useState<HarmonyTitle>(HarmonyTitle.Analogous),
       [harmony, setHarmony] = useHarmony(),
-      [hex, setHex] = useState<string>(color.hex),
       [satVal, setSatVal] = useState<number>(harmony.mainColor.hsl.s * 100),
       [lumVal, setLumVal] = useState<number>(harmony.mainColor.hsl.l * 100)
+
+   const handleTabClick = (tab: HarmonyTitle) => {
+      setSelectedHarmony(tab)
+   }
 
    const handleHexInput = (hex: string) => {
       setHex(hex)
       setSatVal(color.hsl.s * 100)
       setLumVal(color.hsl.l * 100)
-   }
-
-   const handleTabClick = (tab: HarmonyTitle) => {
-      setSelectedHarmony(tab)
    }
 
    useEffect(() => {
@@ -55,6 +54,12 @@ const ColorContainer: React.FC = () => {
       setHex(newHex)
    }
 
+   const handleHslChange = (ev: any) => {
+      const name = ev.target.name
+      const value = ev.target.value
+      const formatVal = { name, value }
+   }
+
    return (
       <div className="color-container-main">
          <HarmonyMenu setTab={(title: HarmonyTitle) => handleTabClick(title)} />
@@ -65,10 +70,32 @@ const ColorContainer: React.FC = () => {
                <ColorName hex={color.hex} />
                <ColorVals hex={color.hex} onChange={handleHexInput} />
                <HarmonyColors colors={harmony.colors} />
-               <input type="range" value={+satVal} onChange={ev => handleSatChange(ev.target.value)} />
-               <input type="text" value={satVal.toFixed(0)} onChange={ev => handleSatChange(ev.target.value)} />
-               <input type="range" value={+lumVal} onChange={ev => handleLumChange(ev.target.value)} />
-               <input type="text" value={lumVal.toFixed(0)} onChange={ev => handleLumChange(ev.target.value)} />
+               <form action="" onChange={ev => handleHslChange(ev)}>
+                  <input
+                     type="range"
+                     name="saturation"
+                     value={+satVal}
+                     onChange={ev => handleSatChange(ev.target.value)}
+                  />
+                  <input
+                     type="text"
+                     name="saturation"
+                     value={satVal.toFixed(0)}
+                     onChange={ev => handleSatChange(ev.target.value)}
+                  />
+                  <input
+                     type="range"
+                     name="luminance"
+                     value={+lumVal}
+                     onChange={ev => handleLumChange(ev.target.value)}
+                  />
+                  <input
+                     type="text"
+                     name="luminance"
+                     value={lumVal.toFixed(0)}
+                     onChange={ev => handleLumChange(ev.target.value)}
+                  />
+               </form>
             </div>
 
             <div className="right-container">
