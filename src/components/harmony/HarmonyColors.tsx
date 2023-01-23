ddefa5d525f9de4ce510987b1647f5562ecf5ffa
Rimon <rimons2804@gmail.com>
2023-01-23 22:43:53 +0200

    migrated to harmony  components fearutes folder

--- src/components/harmony/HarmonyColors.tsx
+++ src/components/harmony/HarmonyColors.tsx
@@ -1,16 +1,30 @@
 import React from 'react'
 import { ColorType } from '../../types/ColorType'
+import { usePaletteContext } from '../../hooks/usePaletteContext'
+import { Color } from '../../services/color.class'
+import { hex } from '../../services/color.class'
 type HarmonyProps = {
    colors: string[]
 }
 
 const Harmony: React.FC<HarmonyProps> = ({ colors }) => {
+   const { palette, addColor } = usePaletteContext()
+
+   let guid = () => {
+      let s4 = () => {
+         return Math.floor((1 + Math.random()) * 0x10000)
+            .toString(16)
+            .substring(1)
+      }
+      return s4() + s4() + '-' + s4() + '-' + s4() + '-' + s4() + '-' + s4() + s4() + s4()
+   }
+
    return (
       <div className="harmony-colors">
          {colors?.map(color => (
-            <div className="color" style={{ background: color }}>
+            <div key={guid()} className="color" style={{ background: color }}>
                <div className="hex">
-                  <span >{color}</span>
+                  <span onClick={() => addColor(new Color(color))}>{color}</span>
                </div>
             </div>
          ))}
