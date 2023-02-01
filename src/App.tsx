7bf93a2cf3448f401cdd34b359db0db29b0b234d
Rimon <rimons2804@gmail.com>
2023-02-01 12:55:11 +0200

    general changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,17 @@
-import React, { useState, useEffect, useRef } from 'react'
 import ColorContainer from './layouts/ColorContainer'
 import PaletteContainer from './layouts/PaletteContainer'
 import { PaletteProvider } from './hooks/usePaletteContext'
-import ContactCard from './components/examples/ContactCard'
+import { HarmonyProvider } from './hooks/useHarmonyContext'
 function App() {
-   const [usedHex, setUsedHex] = useState<string>('ffffff')
-   const [savedColors, setSavedColors] = useState<string[]>([])
-   const handleHexChange = (hex: string) => {
-      setUsedHex(hex)
-   }
-   const handleSaveToPalette = (hex: string) => {
-      setSavedColors([...savedColors, hex])
-   }
    return (
       <div className="App">
          <PaletteProvider>
-            <ColorContainer />
-            <PaletteContainer hex={usedHex} />
+            <HarmonyProvider>
+               <div className="flex">
+                  <ColorContainer />
+                  <PaletteContainer />
+               </div>
+            </HarmonyProvider>
          </PaletteProvider>
       </div>
    )
--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,6 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './assets/styles/global.scss'
-import { useContext } from 'react'
 import { ColorProvider } from './hooks/useColorContext'
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
    <ColorProvider>
