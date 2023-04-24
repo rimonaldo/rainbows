6b4b23a333da071f791492b0730826669f0ec472
Rimon <rimons2804@gmail.com>
2023-04-24 19:39:55 +0300

    change in config repo name

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
    plugins: [react()],
-   base: process.env.NODE_ENV === 'production' ? 'https://github.com/rimonaldo/rainbows' : '/',
+   base: process.env.NODE_ENV === 'production' ? '/rainbows/' : '/',
 })
