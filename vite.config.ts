e71d6a035bc47d1344426343b5ffdf519c943f88
Rimon <rimons2804@gmail.com>
2023-08-12 12:35:46 +0300

    b

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-   plugins: [react()],
-   build: {
-      outDir: 'docs',
-   },
-   base: '/rainbows/'  // Replace 'repo-name' with your actual repository name.
+  plugins: [react()],
+  
 })
